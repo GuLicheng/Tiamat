e1515b3d198a04f0bf49b491f51dfe1e718e1edc
U-DESKTOP-DN3BA6E\Administrator <576356467@qq.com>
2022-04-17 16:45:30 +0800

    Update

--- lv_py/lowshot.py
+++ lv_py/lowshot.py
@@ -158,14 +158,61 @@ def test_after_one_epoch(args, rank, dataloader, model: Classifier, device, logg
         print(f"{k} = {v}")
 
 
+def test_specialize_class(dataloader, model, device, logger, classes):
 
-def make_prototype(trainset, backbone):
+    device = torch.device("cpu")
+
+    tbar = tqdm(dataloader, dynamic_ncols=True)
+    model = model.eval().to(device)
+    
+    np.set_printoptions(4)
+
+    for idx, sample in enumerate(tbar):
+
+        image, target, sal, gt = \
+            sample["image"].to(device), sample["class"].to(device), sample["saliency"].to(device), sample["semantic"]
+        with torch.no_grad():
+            result = model(image, return_cam = True)
+
+
+        logits = result["pred_cls"]
+        pred = logits.detach()
+
+        cam = result["cam"]
+        cam = model.cam_normalize(cam, image.shape[-2:], pred)[0].argmax(dim=0) + 1
+
+        cam[sal[0] == 0] = 0
+        
+        pre_image = cam.cpu().detach().numpy()
+
+        name = sample["ori_name"][0] 
+
+        for cls_id in classes:
+            if target[0][cls_id] == 1:
+                logger.info(f"name = {name}, pred_cls = {pred.cpu().numpy()}")
+                ls = list(zip(PascalVoc.CAT_LIST, pred.cpu().numpy().flatten()))
+                ls.sort(key=lambda x: x[1])
+                for val in ls:
+                    print(f"{val[0]}: {val[1]:.4f}")
+                cv.imshow("0", PascalVoc.show_image(image))
+                res = F.relu(result["cam"][0][cls_id])
+                res = F.interpolate(res[None, None, :, :], size=image.shape[-2:], mode="bilinear", align_corners=False)[0][0]
+                cv.imshow("1", np.uint8(min_max_norm(res).cpu().detach().numpy() * 255))
+                cv.imshow("3", PascalVoc.decode_segmap(pre_image))
+                cv.waitKey(0)
+                
+
+
+def make_prototype(trainset, backbone, device):
+
+    device = torch.device("cpu")
+    
     cls_tokens = []
-    backbone.cuda()
+    backbone.to(device)
     for sample in trainset:
 
         image = sample["image"]
-        image = image[None, :, :, :].cuda()
+        image = image[None, :, :, :].to(device)
 
         with torch.no_grad():
             cls_token = backbone.forward_backup(image)
@@ -178,12 +225,12 @@ def make_prototype(trainset, backbone):
 
 def main():
 
-    parser = dataset_cfg.MY77()
+    parser = dataset_cfg.laboratory()
 
-    parser.add_argument("--lowshot_txt", default="/home/zeyu_yan/20.txt")
-    parser.add_argument("--dino_pretrain", default="/home/zeyu_yan/.cache/torch/hub/checkpoints/dino_deitsmall16_pretrain.pth")
+    parser.add_argument("--lowshot_txt", default=r"D:\code\Tiamat\ori20.txt")
     parser.add_argument("--imagenet_pretrain", 
-        default="/home/zeyu_yan/.cache/torch/hub/checkpoints/S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_384.npz")
+        # default="/home/zeyu_yan/.cache/torch/hub/checkpoints/S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_384.npz")
+        default=r"D:\ckpt\S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_384.npz")
 
     args = parser.parse_args()
 
@@ -219,151 +266,19 @@ def main():
     backbone.init_params(args.imagenet_pretrain)
     classifier = nn.Conv2d(384, PascalVoc.NUM_CLASSES - 1, 1, bias=False)
 
-    candidate = reversed([
-"2008_001610",
-"2010_001418",
-"2008_003562",
-"2010_002973",
-"2008_007239",
-"2009_005236",
-"2011_001135",
-"2010_003887",
-"2011_000379",
-"2008_001876",
-"2010_004963",
-"2007_007530",
-"2010_001457",
-"2008_007472",
-"2009_001444",
-"2009_000535",
-"2009_000684",
-"2008_006843",
-"2010_001842",
-"2007_000876",
-"2010_005796",
-"2010_002286",
-"2010_000114",
-"2011_002389",
-"2010_005505",
-"2009_002972",
-"2010_005317",
-"2010_005202",
-"2009_001502",
-"2007_007878",
-"2010_002811",
-"2010_003230",
-"2007_009630",
-"2009_004887",
-"2011_003216",
-"2011_000108",
-"2010_001347",
-"2008_005716",
-"2010_001247",
-"2010_000815",
-"2009_003660",
-"2010_005791",
-"2009_003697",
-"2011_000345",
-"2010_002778",
-"2010_005128",
-"2007_008575",
-"2010_004144",
-"2010_001939",
-"2007_009464",
-"2010_002573",
-"2007_003525",
-"2007_007098",
-"2010_004540",
-"2008_001741",
-"2010_004306",
-"2009_005160",
-"2010_001590",
-"2008_004583",
-"2009_001117",
-"2010_003696",
-"2008_002215",
-"2010_000043",
-"2010_001386",
-"2009_004705",
-"2010_000675",
-"2007_006641",
-"2011_000999",
-"2008_005600",
-"2008_005300",
-"2010_002625",
-"2010_004060",
-"2008_002749",
-"2010_005627",
-"2009_003921",
-"2009_000553",
-"2010_003974",
-"2009_002917",
-"2008_002182",
-"2008_007165",
-"2010_004760",
-"2010_003174",
-"2010_004933",
-"2011_001754",
-"2010_001177",
-"2010_000661",
-"2010_005232",
-"2008_000860",
-"2010_003665",
-"2010_000469",
-"2007_008403",
-"2007_006303",
-"2007_009724",
-"2009_004434",
-"2007_003778",
-"2008_002177",
-"2007_004998",
-"2010_001660",
-"2009_001205",
-"2010_004365",
-"2010_005513",
-"2009_002628",
-"2009_001270",
-"2011_000973",
-"2010_000404",
-"2011_000469",
-"2008_001592",
-"2010_000269",
-"2007_005688",
-"2010_004808",
-"2007_001825",
-"2007_000549",
-"2011_000758",
-"2009_005037",
-"2007_003788",
-"2009_001894",
-"2010_003925",
-"2008_005770",
-"2010_003950",
-"2010_000519",
-"2009_003783",
-"2007_002760",
-"2008_002067",
-"2007_000528",
-"2009_005177",
-"2010_005805",
-"2007_009216",
-"2010_005652",
-"2007_001185",
-"2010_001195",
-"2010_004171",
-
-    ])
-
     # prototype
-    weights = make_prototype(trainset, backbone)
+    weights = make_prototype(trainset, backbone, 0)
 
     weights = nn.parameter.Parameter(weights.detach().clone())[:, :, None, None]
     classifier.weight.data = weights
 
     model = Classifier(backbone, classifier)
-    model = model.eval().cuda()
-    test_after_one_epoch(args, 0, testloader, model, 0, None)
+    model = model.eval()
 
+
+    logger = Logger("./")
+    # test_after_one_epoch(args, 0, testloader, model, 0, logger)
+    test_specialize_class(testloader, model, 0, logger, [8])
 
 
 if __name__ == "__main__":
